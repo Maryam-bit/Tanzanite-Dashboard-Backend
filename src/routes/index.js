ffea8278b3d0c3b72f13a2e4e6399b3aad88eb08
Maryam-bit <rajputmaryam5555@gmail.com>
2022-09-18 12:43:26 -0700

    feat: registered  user route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import testsRoutes from "./tests.routes.js";
+import usersRoutes from "./users.routes.js"
 
 const router = Router();
 
 const routes = [
 	{ path: "/tests", route: testsRoutes },
+	{ path: "/users", route: usersRoutes },
 ];
 
 routes.forEach(({ path, route }) => {
