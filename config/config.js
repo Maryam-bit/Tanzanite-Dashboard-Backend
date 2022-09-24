9b52f28d42c2936545d3648893bf5c0bedd9ad06
Maryam-bit <rajputmaryam5555@gmail.com>
2022-09-24 09:07:42 -0700

    Fix: added default value for JWT secret in config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     'MODE': 'Development',
     'PORT': process.env.PORT || 5000,
     'MONGO_URL': process.env.MONGO_URL || 'mongodb://localhost:27017/testDB',
-    'JWT_SECRET': process.env.JWT_SECRET
+    'JWT_SECRET': process.env.JWT_SECRET || "pmsRHuOihHkFQzMo1m"
 }
 
 export default config;
